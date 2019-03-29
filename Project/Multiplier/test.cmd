f93c37dcb110d55c5b6d32ccfd6ddbd5c91c73e7
Manan Sharma <manansharma296@gmail.com>
2019-03-29 14:54:04 +0530

    Added python script to test 4 bit full adder

--- Project/Multiplier/test.cmd
+++
@@ -1,5 +0,0 @@
-w a b cin cout so
-vector In a b cin
-set vlist {000 001 010 011 100 101 110 111}
-foreach vec $vlist {setvector in $vec ; s}
-
--- Project/Multiplier/twoBitTest.cmd
+++
@@ -1,5 +0,0 @@
-w s_0 s_1 cout cin b_0 b_1 a_0 a_1 
-vector In a_1 a_0 b_1 b_0 cin
-set vlist {00000 00100 00010 01101 10110 10111 11011 10001 10011}
-foreach vec $vlist {setvector in $vec ; s}
-
