abf9af26669d4dd6cb101f795cf849a8a2600e2d
Anirudh0707 <yani.bh@gmail.com>
2019-03-29 11:42:59 +0530

    4Bit Full Adder

--- Project/Full Adder/twoBitTest.cmd
+++
@@ -1,5 +0,0 @@
-w a_1 a_0 b_1 b_0 cin cout s_0 s_1
-vector In a_1 a_0 b_1 b_0 cin
-set vlist {00000 00100 01000 01100 10000 10100 11000 11100}
-foreach vec $vlist {setvector in $vec ; s}
-
---
+++ Project/Multiplier/FourBitTest.cmd
@@ -0,0 +1,5 @@
+w s_0 s_1 s_2 s_3 cout cin b_0 b_1 b_2 b_3 a_0 a_1 a_2 a_3 
+vector In a_3 a_2 a_1 a_0 b_3 b_2 b_1 b_0 cin
+set vlist {000000000 001001100 010100010 011011110 100110010 101100001 110010001 111000001 111000011}
+foreach vec $vlist {setvector in $vec ; s}
+
