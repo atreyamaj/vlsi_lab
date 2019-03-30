fa16b4b20b4a8b20db14e40e43b4a9cf6845a8fb
Manan Sharma <manansharma296@gmail.com>
2019-03-30 14:16:00 +0530

    4bit multiplier working(100) accuracy

--- Project/Working_Multiplier_Input rounting_left/Test_Multi.cmd
+++ Project/Working_Multiplier_Input rounting_left/Test_Multi.cmd
@@ -1,4 +1,5 @@
 w p_0 p_1 p_2 p_3 p_4 p_5 p_6 p_7 b_0 b_1 b_2 b_3 a_0 a_1 a_2 a_3 
+stepsize 50
 vector In a_3 a_2 a_1 a_0 b_3 b_2 b_1 b_0
 logfile outputs.log
 set vlist {00000000 00100110 01010001 01101111 10011001 10110000 11001000 11100000 11100001}
