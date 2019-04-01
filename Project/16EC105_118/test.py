84309d6be45a70d03f63764231d87f0ffb36e704
Manan Sharma <manansharma296@gmail.com>
2019-04-01 11:05:06 +0530

    Added final workign project files in Project/16Ec105_118 folder

--- Project/16EC105_118/test.py
+++ Project/16EC105_118/test.py
@@ -8,7 +8,6 @@ if(len(sys.argv)<2):
     print("Error. Please provide the .sim file as command line argument")
 
 cmdfile = "test_file.cmd"
-#sim_file = "multiplier_mux.sim"
 sim_file = sys.argv[1]
 logfile = "output.log"
 in1_vector = "a_3 a_2 a_1 a_0"
@@ -16,9 +15,10 @@ in2_vector = "b_3 b_2 b_1 b_0"
 sel_vector = "sel cin"
 p_vector = "p_7 p_6 p_5 p_4 p_3 p_2 p_1 p_0"
 out = "out_7 out_6 out_5 out_4 out_3 out_2 out_1 out_0"
+s_vector = "s_3 s_2 s_1 s_0"
 totalnumofvectors = 2**8
 counter = 0
-
+maxsteppower = 0
 def evaluate(eachline):
     global counter
     a_0_txt = eachline[eachline.find('a_0')+4] 
@@ -39,19 +39,10 @@ def evaluate(eachline):
     out_5_txt = eachline[eachline.find('out_5')+6]
     out_6_txt = eachline[eachline.find('out_6')+6]
     out_7_txt = eachline[eachline.find('out_7')+6]
-    p_0_txt = eachline[eachline.find('p_0')+4]
-    p_1_txt = eachline[eachline.find('p_1')+4]
-    p_2_txt = eachline[eachline.find('p_2')+4]
-    p_3_txt = eachline[eachline.find('p_3')+4]
-    p_4_txt = eachline[eachline.find('p_4')+4]
-    p_5_txt = eachline[eachline.find('p_5')+4]
-    p_6_txt = eachline[eachline.find('p_6')+4]
-    p_7_txt = eachline[eachline.find('p_7')+4]
     a = int(a_3_txt+a_2_txt+a_1_txt+a_0_txt,2)
     b = int(b_3_txt+b_2_txt+b_1_txt+b_0_txt,2)
-    
     if(out_7_txt == 'X' or out_6_txt == 'X' or out_5_txt == 'X' or out_4_txt == 'X' or out_3_txt == 'X' or out_2_txt == 'X' or out_1_txt == 'X' or out_0_txt == 'X'):
-        print("dont cares!!")
+        print("Don't Cares present")
         print(out_7_txt+out_6_txt+out_5_txt+out_4_txt+out_3_txt+out_2_txt+out_1_txt+out_0_txt)
     
     else:
@@ -69,20 +60,6 @@ def evaluate(eachline):
             else:
                 print("Success " +  str(a) + "*" + str(b) + "=" + str(product))
                 counter = counter + 1
-            '''
-            result = int(p_7_txt+p_6_txt+p_5_txt+p_4_txt+p_3_txt+p_2_txt+p_1_txt+p_0_txt,2)
-            product = int(a*b)
-            if(result!=product):
-                print("******************************************")
-                print("Error")
-                print(str(a)+"*"+str(b)+ " failed")
-                print('Obtained ' + p_7_txt+p_6_txt+p_5_txt+p_4_txt+p_3_txt+p_2_txt+p_1_txt+p_0_txt + " = "+ str(result))
-                print("Expected " + bin(product)[2:].zfill(8) + " = " + str(product))
-                print("******************************************")
-                #sys.exit()
-            else:
-                print("Success " +  str(a) + "*" + str(b) + "=" + str(product))
-            '''
         else:
             if(cin_txt=='0'):
                 result = int(out_4_txt+out_3_txt+out_2_txt+out_1_txt+out_0_txt,2)
@@ -116,16 +93,24 @@ def evaluate(eachline):
                     counter = counter + 1
 
 with open(cmdfile,"w") as file:
+    file.write("powlogfile powerlog.log\n")
+    file.write("vsupply 5\n")
+    file.write("h vdd\n")
+    file.write("l gnd\n")
+    file.write("powtrace vdd gnd "+ out + " " + sel_vector+" " + p_vector+" "+s_vector+" "+in1_vector + " "+ in2_vector+"\n")
     file.write("logfile "+ logfile +"\n")
-    file.write("stepsize 1000\n")
-    file.write("w " + out + " " + sel_vector + " "+p_vector + " "+in1_vector + " "+ in2_vector + "\n")
+    file.write("stepsize 13.6\n")
+    file.write("w " + out + " " + sel_vector + " "+p_vector + " "+s_vector+" "+in1_vector + " "+ in2_vector + "\n")
     file.write("vector In "+ sel_vector + " " + in1_vector + " "+ in2_vector + "\n")
+    file.write("powstep\n")
     file.write("set vlist {")
     for j in range(0,4,1):
         for i in range(0,totalnumofvectors,1):
             file.write(str(bin(j)[2:].zfill(2))+str(bin(i)[2:].zfill(8)) + " ")
     file.write("}\n")
     file.write("foreach vec $vlist {setvector in $vec ; s}\n")
+    file.write("powlogfile\n")
+    file.write("sumcap\n")
     file.write("logfile\n")
     file.close()
 
@@ -141,9 +126,19 @@ with open(logfile,"r") as fp:
             evaluate(eachline)
             line = fp.readline()
             eachline = ""
+        elif('step = ' in line):
+            steppower_txt = line[line.find('step = ')+7:line.find('step = ')+14]
+            print("Step power ="+ steppower_txt)
+            steppower = float(steppower_txt)
+            if(steppower>maxsteppower):
+                maxsteppower=steppower
+            line=fp.readline()
+            eachline = ""
         else:
             eachline = eachline + line
             line = fp.readline()
     fp.close()
 
 print("Accuracy " + str(counter/(4*totalnumofvectors)*100) + "%")
+print("Maximum Dynamic Power Consumed = "+ str(maxsteppower) + " mW")
+print("Power report: \n" + eachline)
