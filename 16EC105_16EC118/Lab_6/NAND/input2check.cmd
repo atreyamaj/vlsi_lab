e4e1108ecd4f82277a0fb9fdac07810165c2f347
Manan Sharma <manansharma296@gmail.com>
2019-04-17 13:17:34 +0530

    Updated files for Lab Report

--- 16EC105_16EC118/Lab_6/NAND/input2check.cmd
+++
@@ -1,6 +0,0 @@
-w InA InB Out
-d
-l InA InB
-vector In InA InB
-set vlist {00 01 10 11}
-foreach vec $vlist {setvector In $vec; s}
--- 16EC105_16EC118/Lab_6/New Folder/input2check.cmd
+++
@@ -1,6 +0,0 @@
-w a b z
-d
-l a b
-vector In a b
-set vlist {00 01 10 11}
-foreach vec $vlist {setvector In $vec; s}
